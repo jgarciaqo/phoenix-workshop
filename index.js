f9aebc431aebab7b774c3eeebeb432dfa1bdba09
Esther Strom <estrom@quikorder.com>
2016-01-13 15:32:35 -0600

    First commit

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ var express = require('express')
 var app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello from 5000 restart')
+})
+
+app.get('/products', function (req, res) {
+    res.send("Products")
+})
+
+app.get('/about-us', function (req, res) {
+    res.send("About us")
+})
+
+app.get('/contact', function (req, res) {
+    res.send("Contact")
 })
  
-app.listen(3000)
+app.listen(5000)
 
