6bd3b7496b78a36438f20e8eeb7009d8a2db0941
Sean Courtney <scourtney@quikorder.com>
2016-01-13 15:50:20 -0600

    Account

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World from');
 });
 
-app.get('/about-us', function(req, res) {
-   res.send('About Us'); 
+app.get('/account', function(req, res) {
+   res.send('Account'); 
 });
 
 app.listen(5000);
