cf82fc96ff1c8bdecd5e6aa193351fc9b672c649
Jaime Garcia <jgarciaqo@gmail.com>
2016-01-13 15:36:16 -0600

    start project

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var express = require('express')
 var app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
 
+app.get('/', function (req, res) {
+  res.send('Hello World from 5000 using nodemon')
+})
+
+app.listen(5000)
